b737094b8380f08aa2a0b42b6cb637ed3aa16f94
Theo Sun <theo.sun.github@outlook.com>
2024-03-02 10:43:03 +0800

    chore: update

--- app/apis/jobs.go
+++ app/apis/jobs.go
@@ -3,5 +3,10 @@ package apis
 import "github.com/gin-gonic/gin"
 
 func JobAPIs(group *gin.RouterGroup, c *APIBootstrapContext) {
+	group.GET("/task", func(ctx *gin.Context) {
 
+	})
+	group.POST("/task", func(ctx *gin.Context) {
+
+	})
 }
